2eea9b3ff144f70f688f77cae44a493143143f2f
Roberto Velazquez <rovela@ciencias.unam.mx>
2022-08-01 12:01:49 -0500

    Histograma

--- double_well/kinetics_transitions.py
+++ double_well/kinetics_transitions.py
@@ -1,12 +1,41 @@
 
-from tokenize import Double
 
-
+#leemos la base de datos de la trayectoria
 with open("double_well/traj.txt", "r") as datos:
-    lineas = datos.readlines
-    print(type(lineas))
+    lineas = datos.readlines()
+    #print(type(lineas))
     valores = []
-    for linea in lineas:
-        valores.append([Double(x) for x in linea.strip().split(" ")])
-    print(valores)
+    #sacamos los datos de las posiciones y el tiempo
+    #guardamos en la variable valores
+    for i in range(1,len(lineas)):
+        valores.append([float(x) for x in lineas[i].strip().split(" ")])
+    #print(valores[0][1])
 
+    #Sacamos los intervalos 
+    h_x = (max(valores[1][:])-min(valores[1][:]))
+    #i = 99999
+    #j = 3
+    #print("Valor de (" + str(i) + "," + str(j) + "): " + str(valores[i][j]))
+    print("max x " + str(max(valores[:][1])))
+    print("max y " + str(max(valores[:][2])))
+    print("max z " + str(max(valores[:][3])))
+    print("min x " + str(min(valores[:][1])))
+    print("min y " + str(min(valores[:][2])))
+    print("min z " + str(min(valores[:][3])))
+    #Los intervalos serán de -1 amstrong a 7 amstrings
+    n = 1000 #número de intervalos
+    a = -1.0 #límite inferior
+    b = 7.0  #límite superior
+    h = (b-a)/n #ancho de los intervalos
+    m_x = [0]*n #arreglo para el histograna
+    m_y = [0]*n
+    m_z = [0]*n
+    for i in range(n):
+        for j in range(len(valores)):
+            if valores[j][1] >= a+i*h and valores[j][1] < a+(i+1)*h:
+                m_x[i] += 1
+            if valores[j][2] >= a+i*h and valores[j][2] < a+(i+1)*h:
+                m_y[i] += 1
+            if valores[j][3] >= a+i*h and valores[j][3] < a+(i+1)*h:
+                m_z[i] += 1
+        print(str(m_x[i]) + " " + str(m_y[i]) + " " + str(m_z[i]))
