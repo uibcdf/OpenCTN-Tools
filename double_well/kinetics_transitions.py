74923fbc9d9a0101f3df1d19ae9b1ccb16afcf55
Roberto Velazquez <rovela@ciencias.unam.mx>
2022-08-16 18:53:38 -0500

    Discretización de la trajectoria

--- double_well/kinetics_transitions.py
+++ double_well/kinetics_transitions.py
@@ -16,7 +16,7 @@ with open("double_well/traj.txt", "r") as datos:
     #i = 99999
     #j = 3
     #print("Valor de (" + str(i) + "," + str(j) + "): " + str(valores[i][j]))
-    #Los intervalos serán de -1 amstrong a 7 amstrings
+    #Los intervalos serán de a hasta b
     n = 1000 #número de intervalos
     a = -7.0 #límite inferior
     b = 7.0  #límite superior
@@ -24,6 +24,7 @@ with open("double_well/traj.txt", "r") as datos:
     m_x = [0]*n #arreglo para el histograna
     m_y = [0]*n
     m_z = [0]*n
+    traj_dis = [[0,0,0]*len(valores)]
     for i in range(n):
         for j in range(len(valores)):
             if valores[j][1] >= a+i*h and valores[j][1] < a+(i+1)*h:
@@ -32,4 +33,15 @@ with open("double_well/traj.txt", "r") as datos:
                 m_y[i] += 1
             if valores[j][3] >= a+i*h and valores[j][3] < a+(i+1)*h:
                 m_z[i] += 1
-        print(str(m_x[i]) + " " + str(m_y[i]) + " " + str(m_z[i]))
+        #print(str(m_x[i]) + " " + str(m_y[i]) + " " + str(m_z[i]))
+    
+    #Discretización de la trayectoria
+    for i in range(len(valores)):
+        for j in range(n):
+            if valores[i][1] >= a+j*h and valores[i][1] < a+(j+1)*h:
+                traj_dis[j][1] = j
+            if valores[i][2] >= a+j*h and valores[i][2] < a+(j+1)*h:
+                traj_dis[j][2] = j
+            if valores[i][3] >= a+j*h and valores[i][3] < a+(j+1)*h:
+                traj_dis[j][3] = j
+        print(str(traj_dis[i][1]) + " " + str(traj_dis[i][2]) + " " + str(traj_dis[i][3]))
